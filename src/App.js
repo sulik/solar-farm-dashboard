8da19d7f61f7a1a94de3ce5b61168e36dc3b02ed
Olav Viitmann <olav.viitmann@tallink.ee>
2019-07-26 00:14:45 +0300

    Remove not needed files and code

--- src/App.js
+++ src/App.js
@@ -1,23 +1,9 @@
-import './App.css'
-import logo from './logo.svg'
 import React from 'react'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img alt="logo" className="App-logo" src={logo}/>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          rel="noopener noreferrer"
-          target="_blank">
-          Learn React
-        </a>
-      </header>
+      <h1>Solar Farm Dashboard</h1>
     </div>
   )
 }
