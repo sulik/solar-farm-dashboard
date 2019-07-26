0d13bed561775b1097db59112fa307e5df4eff79
Olav Viitmann <olav.viitmann@tallink.ee>
2019-07-26 11:26:03 +0300

    Add weather forecast API response data

---
+++ src/data/weatherForecastMock.js
@@ -0,0 +1,192 @@
+// http://api.planetos.com/v1/datasets/bom_access-g_global_40km/point?lon=-50.5&lat=49.5&apikey=<apikey>&var=av_swsfcdown,av_ttl_cld&count=18
+
+export default {
+  'stats': {
+    'timeMin':    '2019-07-26T00:00:00',
+    'count':      18,
+    'offset':     0,
+    'nextOffset': 18,
+    'timeMax':    '2019-08-05T00:00:00'
+  },
+  'entries': [{
+    'context':     'reference_time_time_lat_lon',
+    'classifiers': { 'reference_time': '2019-07-26T00:00:00.000Z' },
+    'axes':        {
+      'reftime':   '2019-07-26T00:00:00',
+      'time':      '2019-07-26T00:00:00',
+      'latitude':  49.453125,
+      'longitude': -50.625
+    },
+    'data': { 'av_swsfcdown': 42.07725524902344, 'av_ttl_cld': 0.84175 }
+  }, {
+    'context':     'reference_time_time_lat_lon',
+    'classifiers': { 'reference_time': '2019-07-26T00:00:00.000Z' },
+    'axes':        {
+      'reftime':   '2019-07-26T00:00:00',
+      'time':      '2019-07-26T03:00:00',
+      'latitude':  49.453125,
+      'longitude': -50.625
+    },
+    'data': { 'av_swsfcdown': 0.0, 'av_ttl_cld': 1.0 }
+  }, {
+    'context':     'reference_time_time_lat_lon',
+    'classifiers': { 'reference_time': '2019-07-26T00:00:00.000Z' },
+    'axes':        {
+      'reftime':   '2019-07-26T00:00:00',
+      'time':      '2019-07-26T06:00:00',
+      'latitude':  49.453125,
+      'longitude': -50.625
+    },
+    'data': { 'av_swsfcdown': 0.0, 'av_ttl_cld': 0.5841 }
+  }, {
+    'context':     'reference_time_time_lat_lon',
+    'classifiers': { 'reference_time': '2019-07-26T00:00:00.000Z' },
+    'axes':        {
+      'reftime':   '2019-07-26T00:00:00',
+      'time':      '2019-07-26T09:00:00',
+      'latitude':  49.453125,
+      'longitude': -50.625
+    },
+    'data': { 'av_swsfcdown': 8.989013671875, 'av_ttl_cld': 1.0 }
+  }, {
+    'context':     'reference_time_time_lat_lon',
+    'classifiers': { 'reference_time': '2019-07-26T00:00:00.000Z' },
+    'axes':        {
+      'reftime':   '2019-07-26T00:00:00',
+      'time':      '2019-07-26T12:00:00',
+      'latitude':  49.453125,
+      'longitude': -50.625
+    },
+    'data': { 'av_swsfcdown': 190.191650390625, 'av_ttl_cld': 1.0 }
+  }, {
+    'context':     'reference_time_time_lat_lon',
+    'classifiers': { 'reference_time': '2019-07-26T00:00:00.000Z' },
+    'axes':        {
+      'reftime':   '2019-07-26T00:00:00',
+      'time':      '2019-07-26T15:00:00',
+      'latitude':  49.453125,
+      'longitude': -50.625
+    },
+    'data': { 'av_swsfcdown': 495.13482666015625, 'av_ttl_cld': 1.0 }
+  }, {
+    'context':     'reference_time_time_lat_lon',
+    'classifiers': { 'reference_time': '2019-07-26T00:00:00.000Z' },
+    'axes':        {
+      'reftime':   '2019-07-26T00:00:00',
+      'time':      '2019-07-26T18:00:00',
+      'latitude':  49.453125,
+      'longitude': -50.625
+    },
+    'data': { 'av_swsfcdown': 595.7110595703125, 'av_ttl_cld': 1.0 }
+  }, {
+    'context':     'reference_time_time_lat_lon',
+    'classifiers': { 'reference_time': '2019-07-26T00:00:00.000Z' },
+    'axes':        {
+      'reftime':   '2019-07-26T00:00:00',
+      'time':      '2019-07-26T21:00:00',
+      'latitude':  49.453125,
+      'longitude': -50.625
+    },
+    'data': { 'av_swsfcdown': 312.582275390625, 'av_ttl_cld': 1.0 }
+  }, {
+    'context':     'reference_time_time_lat_lon',
+    'classifiers': { 'reference_time': '2019-07-26T00:00:00.000Z' },
+    'axes':        {
+      'reftime':   '2019-07-26T00:00:00',
+      'time':      '2019-07-27T00:00:00',
+      'latitude':  49.453125,
+      'longitude': -50.625
+    },
+    'data': { 'av_swsfcdown': 38.49951171875, 'av_ttl_cld': 1.0 }
+  }, {
+    'context':     'reference_time_time_lat_lon',
+    'classifiers': { 'reference_time': '2019-07-26T00:00:00.000Z' },
+    'axes':        {
+      'reftime':   '2019-07-26T00:00:00',
+      'time':      '2019-07-27T03:00:00',
+      'latitude':  49.453125,
+      'longitude': -50.625
+    },
+    'data': { 'av_swsfcdown': 0.0, 'av_ttl_cld': 1.0 }
+  }, {
+    'context':     'reference_time_time_lat_lon',
+    'classifiers': { 'reference_time': '2019-07-26T00:00:00.000Z' },
+    'axes':        {
+      'reftime':   '2019-07-26T00:00:00',
+      'time':      '2019-07-27T06:00:00',
+      'latitude':  49.453125,
+      'longitude': -50.625
+    },
+    'data': { 'av_swsfcdown': 0.0, 'av_ttl_cld': 1.0 }
+  }, {
+    'context':     'reference_time_time_lat_lon',
+    'classifiers': { 'reference_time': '2019-07-26T00:00:00.000Z' },
+    'axes':        {
+      'reftime':   '2019-07-26T00:00:00',
+      'time':      '2019-07-27T09:00:00',
+      'latitude':  49.453125,
+      'longitude': -50.625
+    },
+    'data': { 'av_swsfcdown': 8.2064208984375, 'av_ttl_cld': 1.0 }
+  }, {
+    'context':     'reference_time_time_lat_lon',
+    'classifiers': { 'reference_time': '2019-07-26T00:00:00.000Z' },
+    'axes':        {
+      'reftime':   '2019-07-26T00:00:00',
+      'time':      '2019-07-27T12:00:00',
+      'latitude':  49.453125,
+      'longitude': -50.625
+    },
+    'data': { 'av_swsfcdown': 191.857177734375, 'av_ttl_cld': 1.0 }
+  }, {
+    'context':     'reference_time_time_lat_lon',
+    'classifiers': { 'reference_time': '2019-07-26T00:00:00.000Z' },
+    'axes':        {
+      'reftime':   '2019-07-26T00:00:00',
+      'time':      '2019-07-27T15:00:00',
+      'latitude':  49.453125,
+      'longitude': -50.625
+    },
+    'data': { 'av_swsfcdown': 551.6188354492188, 'av_ttl_cld': 0.96875 }
+  }, {
+    'context':     'reference_time_time_lat_lon',
+    'classifiers': { 'reference_time': '2019-07-26T00:00:00.000Z' },
+    'axes':        {
+      'reftime':   '2019-07-26T00:00:00',
+      'time':      '2019-07-27T18:00:00',
+      'latitude':  49.453125,
+      'longitude': -50.625
+    },
+    'data': { 'av_swsfcdown': 546.160400390625, 'av_ttl_cld': 0.96875 }
+  }, {
+    'context':     'reference_time_time_lat_lon',
+    'classifiers': { 'reference_time': '2019-07-26T00:00:00.000Z' },
+    'axes':        {
+      'reftime':   '2019-07-26T00:00:00',
+      'time':      '2019-07-27T21:00:00',
+      'latitude':  49.453125,
+      'longitude': -50.625
+    },
+    'data': { 'av_swsfcdown': 291.6417236328125, 'av_ttl_cld': 1.0 }
+  }, {
+    'context':     'reference_time_time_lat_lon',
+    'classifiers': { 'reference_time': '2019-07-26T00:00:00.000Z' },
+    'axes':        {
+      'reftime':   '2019-07-26T00:00:00',
+      'time':      '2019-07-28T00:00:00',
+      'latitude':  49.453125,
+      'longitude': -50.625
+    },
+    'data': { 'av_swsfcdown': 58.680419921875, 'av_ttl_cld': 0.6875 }
+  }, {
+    'context':     'reference_time_time_lat_lon',
+    'classifiers': { 'reference_time': '2019-07-26T00:00:00.000Z' },
+    'axes':        {
+      'reftime':   '2019-07-26T00:00:00',
+      'time':      '2019-07-28T03:00:00',
+      'latitude':  49.453125,
+      'longitude': -50.625
+    },
+    'data': { 'av_swsfcdown': 0.0, 'av_ttl_cld': 0.015625 }
+  }]
+}
