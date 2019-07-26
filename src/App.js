a292a9f2754f1514832562ad21465d352fe000fc
Olav Viitmann <olav.viitmann@tallink.ee>
2019-07-27 02:08:47 +0300

    Put solar panels data to local state, use it with SolarPanels component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
-import React from 'react'
+import * as solarPanelsDataGen from './data/solarPanelsDataGenerator'
+import React, { useState } from 'react'
+import SolarPanels from './components/SolarPanels/SolarPanels'
+
+const solarPanelsDataConfig = solarPanelsDataGen.generateConfig(30)
 
 function App() {
+  const [solarPanelsData] = useState(solarPanelsDataGen.generateData(solarPanelsDataConfig))
+
   return (
-    <div className="App">
+    <div className="solar-farm-dashboard">
       <h1>Solar Farm <span>Dashboard</span></h1>
+      <SolarPanels data={solarPanelsData}/>
     </div>
   )
 }
