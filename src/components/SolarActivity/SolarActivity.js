21873c8c017abb76ef62b5fe1e9a9d3e6ad69190
Olav Viitmann <olav.viitmann@tallink.ee>
2019-07-27 19:11:08 +0300

    Create component to show weather forecast data about solar activity

---
+++ src/components/SolarActivity/SolarActivity.js
@@ -0,0 +1,20 @@
+import React, { useContext } from 'react'
+import { WeatherContext } from '../../utils/WeatherContext'
+
+function SolarActivity() {
+  const { data } = useContext(WeatherContext)
+  const currentValue = data.length !== 0 && data[0].data['av_swsfcdown']
+
+  return (
+    <div className="solar-activity pane">
+      <div>
+        <div className="value">
+          {currentValue} W / m<sup>2</sup>
+        </div>
+        <div>Visible Diffuse Down Solar Flux</div>
+      </div>
+    </div>
+  )
+}
+
+export default SolarActivity
