f3e058844197d622526143f61cb9c63ea5ffd172
Olav Viitmann <olav.viitmann@tallink.ee>
2019-07-27 19:04:55 +0300

    Use weather context provider for weather forecast data

--- src/index.js
+++ src/index.js
@@ -3,16 +3,21 @@ import * as solarPanelsDataGen from './data/solarPanelsDataGenerator'
 import App from './App'
 import React from 'react'
 import ReactDOM from 'react-dom'
+import weatherForecastMock from './data/weatherForecastMock'
 import { SolarPanelsContextProvider } from './utils/SolarPanelsContext'
+import { WeatherContextProvider } from './utils/WeatherContext'
 
 const solarPanelsDataConfig = solarPanelsDataGen.generateConfig(30)
 
 function Index() {
   const solarPanelsInitialData = solarPanelsDataGen.generateData(solarPanelsDataConfig)
+  const weatherInitialData = weatherForecastMock.entries
 
   return (
     <SolarPanelsContextProvider data={solarPanelsInitialData}>
-      <App solarPanelsDataConfig={solarPanelsDataConfig}/>
+      <WeatherContextProvider data={weatherInitialData}>
+        <App solarPanelsDataConfig={solarPanelsDataConfig}/>
+      </WeatherContextProvider>
     </SolarPanelsContextProvider>
   )
 }
