bd7d455cfc70cfb7f78f854d1dab2fc4a521c01c
Olav Viitmann <olav.viitmann@tallink.ee>
2019-07-29 17:05:59 +0300

    Use configurations from config context

--- src/App.js
+++ src/App.js
@@ -7,14 +7,26 @@ import SolarActivity from './components/SolarActivity/SolarActivity'
 import SolarPanels from './components/SolarPanels/SolarPanels'
 import Totals from './components/Totals/Totals'
 import useInterval from './utils/useInterval'
+import weatherForecastMock from './data/weatherForecastMock'
+import { ConfigContext } from './utils/ConfigContext'
+import { getWeatherData, prepareWeatherData } from './utils/data'
 import { SolarPanelsContext } from './utils/SolarPanelsContext'
+import { WeatherContext } from './utils/WeatherContext'
 
 function App({ solarPanelsDataConfig }) {
   const { setData: setSolarPanelsData } = useContext(SolarPanelsContext)
+  const { setData: setWeatherData } = useContext(WeatherContext)
+  const { config, setWeatherApiEnabled } = useContext(ConfigContext)
 
   useInterval(() => {
     setSolarPanelsData(solarPanelsDataGen.generateData(solarPanelsDataConfig))
-  }, 10 * 1000)
+  }, config.solarPanelsInterval)
+
+  useInterval(async() => {
+    const weatherData = config.weatherApiEnabled ? await getWeatherData() : weatherForecastMock
+    if (weatherData) setWeatherData(prepareWeatherData(weatherData.entries))
+    else setWeatherApiEnabled(false)
+  }, config.weatherInterval)
 
   return (
     <div className="solar-farm-dashboard">
