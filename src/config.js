366c300ebf645caf1f103f2d1e16a67e3547cfe2
Olav Viitmann <olav.viitmann@tallink.ee>
2019-07-29 16:38:42 +0300

    Create api and defaut app config

---
+++ src/config.js
@@ -0,0 +1,10 @@
+export const appConfig = {
+  solarPanelsInterval: 10 * 1000,
+  weatherInterval:     5 * 60 * 1000,
+  weatherApiEnabled:   false
+}
+
+export const apiConfig = {
+  weatherApiKey: 'write-your-api-key-here',
+  weatherApiUrl: 'http://api.planetos.com/v1/datasets/bom_access-g_global_40km/point'
+}
