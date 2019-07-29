ef5d8b6170c7cbea56afb5d58042e9f672bfe3be
Olav Viitmann <olav.viitmann@tallink.ee>
2019-07-29 15:45:16 +0300

    Use charts to visualize total power and energy

--- src/components/Totals/Totals.js
+++ src/components/Totals/Totals.js
@@ -1,13 +1,20 @@
+import Chart from '../../utils/Chart'
 import React, { useContext } from 'react'
 import { SolarPanelsContext } from '../../utils/SolarPanelsContext'
 
 function Totals() {
   const { totals } = useContext(SolarPanelsContext)
-  const { power, energy } = totals
+  const { power, energy } = totals[totals.length - 1]
 
   return (
     <div className="totals pane">
       <h2>Total</h2>
+      <Chart
+        data={totals}
+        dataKey="energy"
+        height="100%"
+        name="Energy"
+        unit="kWh"/>
       <div className="details">
         <div>
           <div className="value">
