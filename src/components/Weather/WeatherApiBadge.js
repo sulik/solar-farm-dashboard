3e070f27f31a1d0afe4e87274990ce570bb86c5d
Olav Viitmann <olav.viitmann@tallink.ee>
2019-07-30 16:09:07 +0300

    Move WeatherApiBadge under Weather

---
+++ src/components/Weather/WeatherApiBadge.js
@@ -0,0 +1,17 @@
+import React, { useContext } from 'react'
+import { ConfigContext } from '../../utils/ConfigContext'
+
+function WeatherApiBadge() {
+  const { config } = useContext(ConfigContext)
+  const { weatherApiEnabled } = config
+  const className = weatherApiEnabled ? 'green' : 'red'
+  const text = weatherApiEnabled ? 'Weather API' : 'Weather Mock'
+
+  return (
+    <div className={'badge ' + className}>
+      {text}
+    </div>
+  )
+}
+
+export default WeatherApiBadge
